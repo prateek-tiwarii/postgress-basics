cf34196705702d760805c82bf444b08e91cb176e
prateek tiwari <tiwariprateek1976@gmail.com>
2025-09-07 22:39:30 +0530

    practice

--- app/api/getTodos/route.ts
+++ app/api/getTodos/route.ts
@@ -1,4 +1,5 @@
 import { Prisma, PrismaClient } from "@prisma/client/extension";
+import { Car } from "lucide-react";
 import { NextRequest, NextResponse } from "next/server";
 
 const client = new PrismaClient();
@@ -6,7 +7,13 @@ const client = new PrismaClient();
 export async function GET(req: NextRequest , res : NextResponse) {
 
     try{
-        const todos = await client.todo.findmany()
+        const todos = await client.todo.findmany();
+
+        return NextResponse.json(todos , {status: 201});
+    }
+    catch(err){
+        return NextResponse.json({message : "something went wrong"}, {status: 401})
+
     }
 
 }
